48c5019a3bc40d4082f00a5e4c2b60d062d5a857
Monika <monarus13@gmail.com>
2024-05-25 21:56:22 +0200

    Added id to model

--- src/Models/CostItem.cs
+++ src/Models/CostItem.cs
@@ -1,9 +1,11 @@
-﻿namespace CostAnalyzer.Models
+﻿using System;
+
+namespace CostAnalyzer.Models
 {
     public class CostItem
     {
+        public Guid Id { get; set; }
         public string Description { get; set; }
-
         public decimal Cost { get; set; } 
         public string[] Tags { get; set; }
     }
