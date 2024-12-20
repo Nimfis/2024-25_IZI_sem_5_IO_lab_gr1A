49975fcaa20c51fb8eea6b2578f1f0be934ce48b
Monika <monarus13@gmail.com>
2024-12-20 20:30:16 +0100

    Added CostItem model and CostItemsRepository with a sample test data.

---
+++ src/Models/CostItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CostAnalyzer.Models
+{
+    public class CostItem
+    {
+        public string Description { get; set; }
+
+        public decimal Cost { get; set; } 
+        public string[] Tags { get; set; }
+    }
+}
---
+++ src/Services/CostItemsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using CostAnalyzer.Models;
+
+namespace CostAnalyzer.Services
+{
+    public class CostItemsRepository
+    {
+        public IEnumerable<CostItem> GetItems() => new[]
+        {
+            new CostItem()
+            {
+                Cost = 12.59m,
+                Description = "Biedronka - Kefir Biedronka - KefirBi edronka - KefirBiedr onka - KefirB iedronka - Kefir Biedro nka - Kefir",
+                Tags = new string[] {"Zakupy", "Jedzenie"}
+            },
+            new CostItem()
+            {
+                Cost = 1023.23m,
+                Description = "Biedronka - Kefir",
+                Tags = new string[] {"Zakupy", "Jedzenie"}
+            },
+            new CostItem()
+            {
+                Cost = 12.5m,
+                Description = "Biedronka - Kefir",
+                Tags = new string[] {"Zakupy", "Jedzenie"}
+            }
+        };
+    }
+}
