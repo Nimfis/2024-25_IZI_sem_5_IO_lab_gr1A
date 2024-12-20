d7516f6f6045a2254fcaf54a256e1aed262c9805
Monika <monarus13@gmail.com>
2024-12-20 22:43:13 +0100

    Added new view for list of costs.

---
+++ src/ViewModels/CostListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CostAnalyzer.Models;
+
+namespace CostAnalyzer.ViewModels
+{
+    public class CostListViewModel : ViewModelBase
+    {
+        public CostListViewModel(IEnumerable<CostItem> items)
+        {
+            Items = new ObservableCollection<CostItem>(items);
+        }
+
+        public ObservableCollection<CostItem> Items { get; }
+    }
+}
