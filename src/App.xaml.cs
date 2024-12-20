d4abd41e2c382ba3bb1aa8ba00ea5592b86aa50a
Monika <monarus13@gmail.com>
2024-12-20 20:22:49 +0100

    Created new Avalonia project

---
+++ src/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using CostAnalyzer.Services;
+using CostAnalyzer.ViewModels;
+using CostAnalyzer.Views;
+
+namespace CostAnalyzer
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            base.OnFrameworkInitializationCompleted();
+
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+            }
+        }
+    }
+}
---
+++ src/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+
+namespace CostAnalyzer.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        ViewModelBase content;
+
+        public ViewModelBase Content
+        {
+            get => content;
+            private set => this.RaiseAndSetIfChanged(ref content, value);
+        }
+    }
+}
